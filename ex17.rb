2943384d2fa833935081e13dcdef473a326f28c0
MAikOL <MAikOL@live.com.mx>
2011-08-13 11:24:51 -0500

    Signed keys y actualización de ejemplos

---
+++ ex17.rb
@@ -0,0 +1,22 @@
+from_file, to_file = ARGV
+script = $0
+
+puts "Copyng from #{from_file} to #{to_file}"
+
+#We could do thes two on one line too, how?
+input = File.open(from_file)
+indata = input.read()
+
+puts "The input file is #{indata.length} bytes long"
+
+puts "Does the output file exist? #{File.exist? to_file}"
+puts "Ready, hit RETURN to continue, CTRL-C to abort."
+STDIN.gets
+
+output = File.open(to_file, 'w')
+output.write(indata)
+
+puts "Alright, all done."
+
+output.close()
+input.close()
---
+++ funciones.rb
@@ -0,0 +1,37 @@
+# this one is like your scripts with argv
+def suma(*args)
+  arg1, arg2 = args
+  arg3 = arg1 + arg2
+  puts "Si sumo: #{arg1} y #{arg2} el resultado es: %d" % arg3
+end
+ 
+# ok, that *args is actually pointless, we can just do this
+def resta(uno, dos)
+  res = uno - dos
+  puts "La resta de #{uno} y #{dos} es: #{res}"
+end
+
+# this just takes one argument
+def nombre(string)
+  puts "Hola #{string} como estas?"
+end
+
+# this one takes no argument
+def string()
+   puts "Hola!!!"
+end
+
+puts "Dame el primer valor de la suma"
+s1 = STDIN.gets.chomp()
+puts "Dame el segundo valor de la suma"
+s2 = STDIN.gets.chomp()
+puts suma(s1, s2)
+puts "Dame el primer valor de la resta"
+r1 = STDIN.gets.chomp()
+puts "Dame el segundo valor de la resta"
+r2 = STDIN.gets.chomp()
+puts resta(r1, r2)
+puts "Cual es tu nombre"
+name = STDIN.gets.chomp()
+puts nombre(name)
+puts string()
